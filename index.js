d10a93ccff329ccbdec845219848ae6696c5a458
Soraiya11-7 <soraiyaparvin@gmail.com>
2025-02-20 14:10:44 +0600

    initial setup is done

---
+++ index.js
@@ -0,0 +1,22 @@
+require("dotenv").config();
+const express = require('express');
+const cors = require('cors');
+const jwt = require('jsonwebtoken');
+const app = express();
+
+const port = process.env.PORT || 5000;
+
+//middleware
+app.use(cors());
+app.use(express.json());
+
+
+
+
+app.get('/', (req, res) => {
+  res.send("server is running...");
+})
+
+app.listen(port, () => {
+  console.log(`server is running on port : ${port}`);
+})
